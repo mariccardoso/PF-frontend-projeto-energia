0e4f0c9f4aeee5e6190d185444c0d3131f1e2ebb
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-05-15 11:47:58 -0300

    feat: create header section

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <header className={styles.header}>
+      <h2>Logo</h2>
+    </header>
+    <section className={styles.mainSection}>
+
+    </section>
+
+    <footer className={styles.footer}>
+
+    </footer>
+    </div> 
+  );
+}
